f353d08394847d33b9e448983546fd4f1c55a0cb
Fredrik Simonsson <simson@thesimson.net>
2019-08-19 00:15:02 +0900

    Added a feature flag for depricated code

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,32 @@
 extern crate  embedded_hal as hal;
 use core::cell::RefCell;
 /// embedded hal spy implemnets call backs for used traits
-/// 
+///
 /// Intended use is chaining over an existing embedded_hal
 /// implementation sniffing all the data. Useful when preparing
 /// for a refacforing and want to collect actual data for unit
 /// test case.
-/// 
+///
 
 /// Blocking SPI API
 /// hal::blocking::spi::Transfer will return
 /// DataWord::First at start of transfer, all data sent in DataWord::Byte(u8)
 /// DataWord::Response to indicate where transmit ends and response begins
 /// all recived bytes in DataWord::Byte(u8) ending with DataWord::Last
-/// 
+///
 /// Usage:
 /// ```
 ///    extern crate embedded_hal_spy;
 ///    use embedded_hal_spy::DataWord;
 /// #   use linux_embedded_hal::Spidev;
 /// #   use linux_embedded_hal::Pin;
-/// 
+///
 /// #   if let Ok(spi) = Spidev::open("/dev/spidev0.0"){
 ///    let mut spix = embedded_hal_spy::new(spi,
 ///             |w|{
 ///                 match w {
 ///                     DataWord::First => {
-///                         print!("data = ["); 
+///                         print!("data = [");
 ///                     },
 ///                     DataWord::Last =>  {println!("],"); },
 ///                     DataWord::Response =>  { print!("],\r\n       ["); },
@@ -44,7 +44,7 @@ use core::cell::RefCell;
 pub struct Spy<T,F>
 where F:Fn(DataWord)
 {
-    /// object implementing emedded hal 
+    /// object implementing emedded hal
     s: RefCell<T>,
     /// Callback
     f: RefCell<F>,
@@ -65,10 +65,10 @@ pub enum DataWord {
     None,
     /// Encapsulate data
     Byte(u8),
-    /// indicates first byte in transaction when used it 
+    /// indicates first byte in transaction when used it
     /// will be followd by last after the last byte
     First,
-    /// When used it is sent after last byte in transaction 
+    /// When used it is sent after last byte in transaction
     Last,
     /// Indicate beggining of response from a tranasction based class
     Response,
@@ -81,7 +81,7 @@ pub enum DataWord {
 use hal::spi::FullDuplex;
 extern crate nb;
 /// FullDuplex will return every data sent and read in DataWord::Byte(u8)
-/// 
+///
 impl<T,F> FullDuplex<u8> for Spy<T,F>
 where T:FullDuplex<u8>,
       F: Fn(DataWord),
@@ -139,7 +139,71 @@ where T: hal::blocking::spi::Write<u8>,
         (self.s.borrow_mut()).write(words)
     }
 }
- 
+#[cfg(feature = "embedded_hal_digital_io_legacy_v1")]
+/// Legacy traits
+/// Digital InputPin
+impl<T,F> hal::digital::v1::InputPin for Spy<T,F>
+where T: hal::digital::v1::InputPin,
+      F: Fn(DataWord)
+ {
+    fn is_high(&self) -> bool{
+        let state = (self.s.borrow_mut()).is_high();
+
+        (self.f.borrow_mut())(DataWord::Byte(state as u8));
+        state
+    }
+    fn is_low(&self) -> bool{
+        let state = (self.s.borrow_mut()).is_low();
+        (self.f.borrow_mut())(DataWord::Byte((!state) as u8));
+        state
+    }
+}
+#[cfg(feature = "embedded_hal_digital_io_legacy_v1")]
+/// Digital OutputPin
+impl<T,F> hal::digital::v1::OutputPin for Spy<T,F>
+where T: hal::digital::v1::OutputPin,
+      F: Fn(DataWord)
+ {
+    fn set_high(&mut self){
+        (self.f.borrow_mut())(DataWord::Byte(1));
+        (self.s.borrow_mut()).set_high()
+    }
+    fn set_low(&mut self){
+        (self.f.borrow_mut())(DataWord::Byte(0));
+        (self.s.borrow_mut()).set_low()
+    }
+}
+#[cfg(feature = "embedded_hal_digital_io_legacy_v1")]
+impl<T,F> hal::digital::v1::ToggleableOutputPin for Spy<T,F>
+where T: hal::digital::v1::ToggleableOutputPin,
+      F: Fn(DataWord)
+ {
+    fn toggle(&mut self){
+        (self.f.borrow_mut())(DataWord::Toggle);
+        (self.s.borrow_mut()).toggle()
+    }
+}
+
+#[cfg(feature = "embedded_hal_digital_io_legacy_v1")]
+impl<T,F> hal::digital::v1::StatefulOutputPin for Spy<T,F>
+where T: hal::digital::v1::StatefulOutputPin,
+      F: Fn(DataWord)
+{
+    fn is_set_high(&self) -> bool{
+        let state = (self.s.borrow_mut()).is_set_high();
+
+        (self.f.borrow_mut())(DataWord::Byte(state as u8));
+        state
+    }
+    fn is_set_low(&self) -> bool{
+        let state = (self.s.borrow_mut()).is_set_low();
+        (self.f.borrow_mut())(DataWord::Byte((!state) as u8));
+        state
+    }
+}
+
+#[cfg(not(feature = "embedded_hal_digital_io_legacy_v1"))]
+// V2 traits
 /// Digital InputPin
 impl<T,F> hal::digital::v2::InputPin for Spy<T,F>
 where T: hal::digital::v2::InputPin,
@@ -157,7 +221,7 @@ where T: hal::digital::v2::InputPin,
         Ok(state)
     }
 }
-
+#[cfg(not(feature = "embedded_hal_digital_io_legacy_v1"))]
 /// Digital OutputPin
 impl<T,F> hal::digital::v2::OutputPin for Spy<T,F>
 where T: hal::digital::v2::OutputPin,
@@ -173,7 +237,7 @@ where T: hal::digital::v2::OutputPin,
         (self.s.borrow_mut()).set_low()
     }
 }
-
+#[cfg(not(feature = "embedded_hal_digital_io_legacy_v1"))]
 impl<T,F> hal::digital::v2::ToggleableOutputPin for Spy<T,F>
 where T: hal::digital::v2::ToggleableOutputPin,
       F: Fn(DataWord)
@@ -185,7 +249,7 @@ where T: hal::digital::v2::ToggleableOutputPin,
     }
 }
 
-
+#[cfg(not(feature = "embedded_hal_digital_io_legacy_v1"))]
 impl<T,F> hal::digital::v2::StatefulOutputPin for Spy<T,F>
 where T: hal::digital::v2::StatefulOutputPin,
       F: Fn(DataWord)
@@ -193,7 +257,7 @@ where T: hal::digital::v2::StatefulOutputPin,
     //type Error = T::Error;
     fn is_set_high(&self) -> Result<bool,T::Error>{
         let state = (self.s.borrow_mut()).is_set_high()?;
-        
+
         (self.f.borrow_mut())(DataWord::Byte(state as u8));
         Ok(state)
     }
